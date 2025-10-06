34647792dbf454e1df2be25a96bf6cf85ee72f0b
sdurutr436 <sdurutr436@g.educaand.es>
2025-10-06 14:07:19 +0200

    Primer commit

---
+++ src/test/java/iesrafaelalberti/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package iesrafaelalberti.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
