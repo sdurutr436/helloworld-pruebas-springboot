406259023370493bc8c071b645450a975dcb732d
sdurutr436 <sdurutr436@g.educaand.es>
2025-10-08 13:05:25 +0200

    Añadido cambios a Task

--- src/main/java/iesrafaelalberti/helloworld/entities/Task.java
+++ src/main/java/iesrafaelalberti/helloworld/entities/Task.java
@@ -1,13 +1,26 @@
 package iesrafaelalberti.helloworld.entities;
 
+import java.util.Date;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Task {
     
     @Id
+    @GeneratedValue (strategy = GenerationType.TABLE)
+    // @SequenceGenerator(name="task_seq", sequenceName="task_seq", allocationSize=1)
     int id;
 
     String name;
+    String description;
+
+    int priority;
+    int status;
+
+    Date dueDate;
+    
 }
